ca1d3012c5738bbda9ce0a8d41c9e36f71187d5e
Anujjadon01 <https://github.com/Anujjadon01/Practice-JavaScript01.git>
2025-01-21 21:35:23 +0530

    assig

---
+++ ha.js
@@ -0,0 +1,85 @@
+// Q=1
+
+// let score=65;
+
+// if(score>=90){
+//     console.log("Grade-A");
+
+// }else if(score>=80 && score<=89){
+//     console.log("Grade-B");
+
+// }else if(score>=70 && score<=79){
+//     console.log("Grade-C");
+
+// }else if(score<70 && score>=50){
+//     console.log("Grade-D");
+
+// }else{
+//     console.log("Grade-F");
+// }
+
+// Q=2
+
+// let a=5;
+// let b=5;
+// let c=7;
+// if(a === b && b === c && a === c){
+//     console.log("Equilateral triangle.");
+    
+// }else if(a === b || b === c || a===c){
+//     console.log("Isosceles triangle.");
+    
+// }else{
+//     console.log("Scalene triangle");
+    
+// }
+
+// Q=3
+
+// let a = 67;
+
+// if (a % 2 === 0) {
+//     if (a > 50) {
+//         console.log("Even and Large");
+
+//     } else {
+//         console.log("Even and Small");
+
+//     }
+
+// } else if (a < 30) {
+
+//     console.log("Odd and Small");
+
+// } else {
+//     console.log("Odd and Large");
+
+// }
+
+
+// Q=1
+// let a=5;
+// for (let i = 1; i <= 10; i++) {
+//     console.log("5","X",i,"=",a*i);  
+// }
+
+// Q=2
+
+// let a=6;
+// let temp=0;
+// for (let i = 1; i <= a; i++) {
+//     temp+=i;
+// }
+// console.log(temp);
+
+// Q=3
+
+// let a=4;
+// for (let i = 1; i <= a; i++) {
+//     let row="";
+//     for (let j = 1; j <= i; j++) {
+//         row+=j;
+//     }
+//     console.log(row);
+// }
+
