940357850f799c61eb85be03ea33c8b8f7c45a36
Anujjadon01 <https://github.com/Anujjadon01/Practice-JavaScript01.git>
2025-01-22 21:19:36 +0530

    Added file Task-2

--- ha.js
+++ ha.js
@@ -20,19 +20,25 @@
 
 // Q=2
 
-// let a=5;
-// let b=5;
-// let c=7;
-// if(a === b && b === c && a === c){
-//     console.log("Equilateral triangle.");
-    
-// }else if(a === b || b === c || a===c){
-//     console.log("Isosceles triangle.");
-    
-// }else{
-//     console.log("Scalene triangle");
-    
+// let a = 1;
+// let b = 1;
+// let c = 1;
+// if (a < 0 && b < 0 && c < 0) {
+//     console.log("invalid");
+
+// } else {
+//     if (a == b && b == c && a == c) {
+//         console.log("Equilateral triangle.");
+
+//     } else if (a == b && c > 0 || b == c && a > 0 || a == c && b > 0) {
+//         console.log("Isosceles triangle.");
+
+//     } else {
+//         console.log("Scalene triangle");
+
+//     }
 // }
+
 
 // Q=3
 
@@ -60,7 +66,7 @@
 // Q=1
 // let a=5;
 // for (let i = 1; i <= 10; i++) {
-//     console.log("5","X",i,"=",a*i);  
+//     console.log("5","X",i,"=",a*i);
 // }
 
 // Q=2
@@ -77,9 +83,108 @@
 // let a=4;
 // for (let i = 1; i <= a; i++) {
 //     let row="";
-//     for (let j = 1; j <= i; j++) {
+//     for (let j = 1; j <=i; j++) {
 //         row+=j;
 //     }
 //     console.log(row);
 // }
+
+
+// for loop q
+
+// Q=1
+
+// let a=5;
+// for (let i = 1; i <=a; i++) {
+//     let str="";
+//     for (let j = 1; j <=i ; j++) {
+//         str+="*";
+
+//     }
+//     console.log(str);
+// }
+
+// Q=2
+
+// let b=5;
+// for (let i = 1; i <=b; i++) {
+//     let tstr="";
+//     for (let j = b; j >=i; j--) {
+//         tstr+="*"
+
+//     }
+//     console.log(tstr);
+
+// }
+
+// Q=3
+
+// let c=9;
+// for (let i = 1; i <=c; i+=2) {
+//     let row="";
+//     for (let j = c; j >=i; j-=2) {
+//         row+=" ";
+
+//     }
+//     for (let k = 1; k <= i; k++) {
+//         row+="*";
+
+//     }
+//     console.log(row);
+// }
+
+
+// Q=4
+
+// let d=5;
+// for (let i = 1; i <=d; i++) {
+//     let ste_n="";
+//     for (let j = 1; j <=i; j++) {
+//         ste_n+=j+" "     
+//     }
+//     console.log(ste_n);   
+// }
+
+// Q=5
+
+// let a = 5;
+// for (let i = a; i >= 1; i--) { 
+//     let str = "";
+//     for (let j = a; j >= a-i+1; j--) {
+//         str += j+" "; 
+//     }
+//     console.log(str); 
+// }
+
+
+// Q=6
+
+// let c = 9;
+// for (let i = 1; i <= c; i += 2) {
+//     let row = "";
+//     for (let j = c; j >= i; j -= 2) {
+//         row += " ";
+//     }
+//     for (let k = 1; k <= i; k++) {
+//         row += "*";
+//     }
+//     console.log(row);
+// }
+// for (let i = c - 2; i >= 1; i -= 2) {
+//     let row = "";
+//     for (let j = c; j >= i; j -= 2) {
+//         row += " ";
+//     }
+//     for (let k = 1; k <= i; k++) {
+//         row += "*";
+//     }
+//     console.log(row);
+// }
+
+
+
+
+
+
+
 
