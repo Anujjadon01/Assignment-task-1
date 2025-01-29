a97996e771d236ea4ef937ff4f0a418b77548c0f
Anuj Jadon <anujthakur745696953@gmail.com>
2025-01-29 21:11:42 +0530

    event task5

---
+++ Assignment-task-1/task5.js
@@ -0,0 +1,47 @@
+const button1=document.getElementById('btn1');
+ 
+ 
+    button1.addEventListener("click",()=>{
+        if(button1.innerHTML==="click-me"){
+            button1.innerHTML="clicked!";
+        }else{
+            button1.innerHTML="click-me";
+        }
+    });
+   
+ 
+const hover=document.getElementById('coh');
+hover.addEventListener("mouseover",()=>{
+    hover.style.backgroundColor ="blue";
+})
+hover.addEventListener("mouseout",()=>{
+    hover.style.backgroundColor ="lightgray";
+})
+
+const input1=document.getElementById('input');
+const pra=document.getElementById('inpra');
+input1.addEventListener("input",()=>{
+    pra.innerText=input1.value; 
+});
+
+
+const clickBtn = document.getElementById('clickMe');
+const disableBtn = document.getElementById('disableClick');
+
+
+function mainclick() {
+    alert('Button Clicked!');
+}
+
+clickBtn.addEventListener("click", mainclick);
+
+disableBtn.addEventListener("click", () => {
+    clickBtn.removeEventListener("click", mainclick);
+    alert("Click event disabled!");
+});
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {  // या event.keyCode === 13 (पुराना तरीका)
+        alert("Enter key was pressed!");
+    }
+});
+
